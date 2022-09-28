220b8d7e6cfb7be3aaac8e042e373c52b1183a5e
Zuber Marcell <zubermarcellpetrik@gmail.com>
2022-09-28 13:46:24 +0200

    dark,light mode, mukodő

--- src/index.js
+++ src/index.js
@@ -2,3 +2,21 @@ import 'bootstrap/dist/js/bootstrap.bundle.js';
 import  'bootstrap/dist/css/bootstrap.css';
 
 import './style.css';
+
+document.getElementById('darkModeButton').addEventListener('click',darkMode);
+
+function darkMode(){
+    if(document.getElementById('darkMode').style.backgroundColor=="white"){
+        document.getElementById('darkMode').style.backgroundColor="black";
+        document.getElementById('darkMode').style.color="white"
+    } else{
+        document.getElementById('darkMode').style.backgroundColor="white";
+        document.getElementById('darkMode').style.color="black"
+    }
+}
+document.getElementById('image')
+
+function init() {
+}
+console.log('loaded')
+document.addEventListener('DOMContentLoaded', init);
